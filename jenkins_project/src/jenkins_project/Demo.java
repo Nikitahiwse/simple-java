7951bc894b50ec6568a61e48992c5e0b9354e483
X240 <X240@DESKTOP-K8HJ363>
2020-10-15 05:29:54 -0700

    simple java project

---
+++ jenkins_project/src/jenkins_project/Demo.java
@@ -0,0 +1,26 @@
+package jenkins_project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+	public static void main(String[] args) throws InterruptedException 
+	{
+		
+		
+			WebDriver wd;
+			System.setProperty("webdriver.chrome.driver", "E:\\chromedriver\\chromedriver_win32\\chromedriver.exe");
+			wd=new ChromeDriver();
+			wd.get("http://automationpractice.com/index.php");
+			Thread.sleep(5000);
+			
+			wd.manage().window().maximize();
+			
+			Thread.sleep(3000);
+
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+}
